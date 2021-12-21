4e14a09364cd2d85987bd795c6723eda8e666dd8
trundler-dev <57276947+trundler-dev@users.noreply.github.com>
2021-12-21 19:07:14 -0400

    Updated weapons

--- code/Weapons/IcePick.cs
+++ code/Weapons/IcePick.cs
@@ -1,6 +1,32 @@
-﻿namespace FrostFight.Weapons
+﻿using Sandbox;
+
+namespace FrostFight.Weapons
 {
-	public class IcePick
+	[Library( "weapon_icepick" )]
+	public partial class IcePick : BaseWeapon
 	{
+		public override string ViewModelPath => "models/weapons/pickaxe/pickaxe_view.vmdl";
+		public override float PrimaryRate => 0.6f;
+
+		public override void Spawn()
+		{
+			base.Spawn();
+
+			SetModel( "models/weapons/pickaxe/pickaxe_world.vmdl" );
+		}
+
+		public override void AttackPrimary()
+		{
+			(Owner as AnimEntity).SetAnimBool( "fire", true );
+
+			base.AttackPrimary();
+		}
+
+		public override void SimulateAnimator( PawnAnimator anim )
+		{
+			anim.SetParam( "holdtype", 4 );
+			anim.SetParam( "holdtype_handedness", 1 );
+			anim.SetParam( "holdtype_pose_hand", 0.07f );
+		}
 	}
 }
