e2e151c7b5b83f0dd5c051ac15d0a20e7ce4047a
Matt9440 <78914427+Matt9440@users.noreply.github.com>
2021-12-22 18:31:27 +0000

    SpectateBlockCamera

---
+++ code/SpectateBlockCamera.cs
@@ -0,0 +1,54 @@
+﻿using Sandbox;
+
+namespace FrostFight
+{
+	public class SpectateBlockCamera : Camera
+	{
+		public AnimEntity Target { get; set; }
+
+		private Angles OrbitAngles;
+		private float OrbitDistance { get; set; } = 320;
+
+		public override void Update()
+		{
+			var pawn = Target ?? Local.Pawn as AnimEntity;
+
+			if ( pawn == null )
+				return;
+
+			Position = pawn.Position;
+			Vector3 targetPos;
+
+			Position += Vector3.Up * (40 * pawn.Scale);
+			Rotation = Rotation.From( OrbitAngles );
+			targetPos = Position + Rotation.Backward * OrbitDistance;
+
+			var tr = Trace.Ray( Position, targetPos ).WorldOnly()
+				.Radius( 8 )
+				.Run();
+
+			Position = tr.EndPos;
+			FieldOfView = 70;
+
+			Viewer = null;
+		}
+
+		public override void BuildInput( InputBuilder input )
+		{
+			OrbitDistance -= input.MouseWheel * 10;
+			OrbitDistance = OrbitDistance.Clamp( 0, 1000 );
+
+			OrbitAngles.yaw += input.AnalogLook.yaw;
+			OrbitAngles.pitch += input.AnalogLook.pitch;
+			OrbitAngles = OrbitAngles.Normal;
+			OrbitAngles.pitch = OrbitAngles.pitch.Clamp( -89, 89 );
+
+			//input.ViewAngles = OrbitAngles;
+
+			//input.Clear();
+			//input.StopProcessing = true;
+
+			base.BuildInput( input );
+		}
+	}
+}
