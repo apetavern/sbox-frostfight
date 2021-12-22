fe3aec25dbd3dcb65ea205dfad0e82c2937d35e7
trundler-dev <57276947+trundler-dev@users.noreply.github.com>
2021-12-22 10:57:55 -0400

    Waiting and Playing Timers + Win Condition Logic

--- code/Game.State.cs
+++ code/Game.State.cs
@@ -13,8 +13,17 @@ namespace FrostFight
 			GameOver
 		}
 
+		public enum Teams
+		{
+			Freezers,
+			Runners
+		}
+
 		[Net] public GameState State { get; set; }
 		[Net] public IList<FrostPlayer> Players { get; set; }
+		[Net] public float WaitingTimer { get; set; } = -1;
+		[Net] public float PlayingTimer { get; set; } = -1;
+		[Net] public Teams WinningTeam { get; set; }
 
 		[Event.Tick]
 		private void OnTick()
@@ -42,22 +51,62 @@ namespace FrostFight
 
 		private void TickWaiting()
 		{
-			// Check for min players count:
-			if ( Client.All.Count >= 4 )
+			if ( WaitingTimer == -1 )
+				WaitingTimer = 30;
+
+			WaitingTimer -= Time.Delta;
+
+			if ( WaitingTimer <= 0 )
 			{
-				AssignRoles();
-				ChangeState( GameState.Playing );
+				if ( Client.All.Count >= 4 )
+				{
+					AssignRoles();
+					ChangeState( GameState.Playing );
+				}
 			}
 		}
 
 		private void TickPlaying()
 		{
+			if ( PlayingTimer == -1 )
+				PlayingTimer = 300;
 
+			PlayingTimer -= Time.Delta;
+
+			// Check if all Non-Freezers are Frozen.
+			var winConditionMet = true;
+			foreach ( FrostPlayer player in Players )
+			{
+				if ( player.IsFreezer ) continue;
+
+				if ( !player.IsFrozen ) winConditionMet = false;
+
+				if ( !winConditionMet ) break;
+			}
+
+			if ( winConditionMet )
+			{
+				WinningTeam = Teams.Freezers;
+				ChangeState( GameState.GameOver );
+			}
+
+			if ( PlayingTimer <= 0 )
+			{
+				WinningTeam = Teams.Runners;
+				ChangeState( GameState.GameOver );
+			}
 		}
 
 		private void TickGameOver()
 		{
+			if ( WinningTeam == Teams.Runners )
+			{
 
+			}
+			else
+			{
+
+			}
 		}
 
 		private void AssignRoles()
