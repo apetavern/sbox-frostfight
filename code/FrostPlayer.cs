ebb42258105be6fc1bdd0f23fbdab2850093d7aa
Matt9440 <78914427+Matt9440@users.noreply.github.com>
2021-12-22 15:52:38 +0000

    [Net] stamina float

--- code/FrostPlayer.cs
+++ code/FrostPlayer.cs
@@ -8,6 +8,7 @@ namespace FrostFight
 		public const float MaxFreezeAmount = 100;
 		[Net] public bool IsFreezer { get; set; } = false;
 		[Net] public float CurrentFreezeAmount { get; private set; }
+		[Net] public float Stamina { get; set; }
 		public IceBlock IceBlock { get; set; }
 		public bool IsFrozen => CurrentFreezeAmount >= MaxFreezeAmount;
 
