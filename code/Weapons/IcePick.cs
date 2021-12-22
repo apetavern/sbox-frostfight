d48543ef189a68bbaab98f0cb4d2e038c351a8c4
Matt9440 <78914427+Matt9440@users.noreply.github.com>
2021-12-22 15:50:49 +0000

    Impact effects

--- code/Weapons/IcePick.cs
+++ code/Weapons/IcePick.cs
@@ -7,7 +7,7 @@ namespace FrostFight.Weapons
 	{
 		public override string ViewModelPath => "models/weapons/pickaxe/pickaxe_view.vmdl";
 		public override float PrimaryRate => 1.2f;
-		public float WeaponReach { get; set; } = 45f;
+		public float WeaponReach { get; set; } = 65f;
 
 		public override void Spawn()
 		{
@@ -29,13 +29,31 @@ namespace FrostFight.Weapons
 			var trace = Trace.Ray( Owner.EyePos, Owner.EyePos + Owner.EyeRot.Forward * WeaponReach ).Ignore( Owner ).Run();
 
 			if ( trace.Entity is IceBlock block )
-				DoDamage( block );
+				DoDamage( block, trace.EndPos );
 		}
 
-		public async void DoDamage( IceBlock block )
+		public async void DoDamage( IceBlock block, Vector3 hitPos )
 		{
 			await GameTask.DelaySeconds( 0.2f );
+
+			ImpactEffects( hitPos );
+
 			block.TakeDamage();
+		}
+
+		[ClientRpc]
+		public void ImpactEffects( Vector3 hitPos )
+		{
+			if ( IsLocalPawn )
+			{
+				_ = new Sandbox.ScreenShake.Perlin();
+			}
+
+			var impactEffect = Particles.Create( "particles/impact.ice.vpcf", hitPos );
+			impactEffect.SetForward( 0, hitPos.Normal );
+
+			DebugOverlay.Sphere( hitPos, 2f, Color.Red, false, 2f );
+			DebugOverlay.Line( hitPos, hitPos + hitPos.Normal, 2f );
 		}
 
 		[ClientRpc]
