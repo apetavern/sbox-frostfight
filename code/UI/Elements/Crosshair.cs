8dff2cca243bd611d2bb2760b89021bd01fadb47
Alex <alex@gu3.me>
2021-12-23 17:05:53 +0000

    Snowball cooldown bar

--- code/UI/Elements/Crosshair.cs
+++ code/UI/Elements/Crosshair.cs
@@ -1,4 +1,5 @@
-﻿using Sandbox;
+﻿using FrostFight.Weapons;
+using Sandbox;
 using Sandbox.UI;
 
 namespace FrostFight.UI.Elements
@@ -6,6 +7,8 @@ namespace FrostFight.UI.Elements
 	[UseTemplate]
 	public class Crosshair : Panel
 	{
+		public Panel CooldownBar { get; set; }
+
 		public Crosshair()
 		{
 			BindClass( "active", () =>
@@ -17,6 +20,23 @@ namespace FrostFight.UI.Elements
 			{
 				return !(Local.Pawn as FrostPlayer).MovementDisabled;
 			} );
+		}
+
+		public override void Tick()
+		{
+			base.Tick();
+
+			if ( Local.Pawn is not FrostPlayer { ActiveChild: FreezeGun freezeGun } )
+			{
+				CooldownBar.RemoveClass( "visible" );
+				return;
+			}
+
+			float t = freezeGun.TimeSinceSecondaryAttack / 3f;
+			t = t.Clamp( 0, 1f );
+			CooldownBar.SetClass( "visible", !t.AlmostEqual( 1 ) );
+
+			CooldownBar.Style.Width = Length.Fraction( t * 2f );
 		}
 	}
 }
