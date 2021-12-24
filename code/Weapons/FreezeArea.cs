e9b333a08e405dc8903190d7663414423d96e029
Alex <alex@gu3.me>
2021-12-24 01:47:09 +0000

    Slightly increase freeze area radius from 10u to 12u

--- code/Weapons/FreezeArea.cs
+++ code/Weapons/FreezeArea.cs
@@ -27,7 +27,7 @@ namespace FrostFight.Weapons
 
 			if ( TimeSinceLastTick > FreezeTickRate )
 			{
-				var hits = Physics.GetEntitiesInSphere( Position, 10f ).Where( ent => ent is FrostPlayer fPlayer && !fPlayer.IsFreezer ).Cast<FrostPlayer>();
+				var hits = Physics.GetEntitiesInSphere( Position, 12f ).Where( ent => ent is FrostPlayer fPlayer && !fPlayer.IsFreezer ).Cast<FrostPlayer>();
 				if ( hits.Any() )
 				{
 					hits.ToList().ForEach( ent => ent.AddFreeze( .25f, Owner ) );
