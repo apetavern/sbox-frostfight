4c9ee0ab8d012abb5534bff6bb38acbcf19c1413
trundler-dev <57276947+trundler-dev@users.noreply.github.com>
2022-01-03 19:25:15 -0400

    0.25 instead of 0.30 freezersCount

--- code/Game.State.cs
+++ code/Game.State.cs
@@ -131,7 +131,7 @@ namespace FrostFight
 
 		private void AssignRoles()
 		{
-			var freezersCount = MathX.CeilToInt( Players.Count * 0.30f );
+			var freezersCount = MathX.CeilToInt( Players.Count * 0.25f );
 			var playersCount = Players.Count;
 
 			foreach ( var player in Players )
